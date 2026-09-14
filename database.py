f43c5cec4bf4e7d8013398b6fbfde7f69c2a63e9
yxsh-dev <yashithamurugan@gmail.com>
2026-09-14 21:27:03 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="654321",
+        password="Your password",
         database="bus_anpr"
     )
 
